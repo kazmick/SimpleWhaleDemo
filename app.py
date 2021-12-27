8962e7b1fcbbb932d1996b61b929f2aeab233587
Mick Kaz <kazmick@gmail.com>
2021-12-27 21:12:56 +0400

    New app urls

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask import request
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello Simple Whale!"
+
+@app.route('/hi')
+def hello_name():
+    response = 'Hello ' + request.args.get('name')
+    return response
+
+@app.route('/query-example')
+def query_example():
+    # if key doesn't exist, returns None
+    language = request.args.get('language')
+
+    return '''<h1>The language value is: {}</h1>'''.format(language)
